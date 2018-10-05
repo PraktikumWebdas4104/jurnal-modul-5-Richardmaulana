d0be6ec02653563561bc381f97f4f284c194ba4a
MUCHAMMAD RICHARD MAULANA <43516487+Richardmaulana@users.noreply.github.com>
2018-10-05 10:24:47 +0700

    regristrasi.php
    Muchmmad Richard Maulana

--- regristrasi.php
+++ regristrasi.php
@@ -4,8 +4,31 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
-
-
-
+	<form method="POST">
+	<table>
+		<tr>
+			<td>Nim</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+	</table>
+	<input type="submit" name="submit">
+</form>
 </body>
 </html>
+
+<?php
+	if (isset($_POST['submit'])) {
+		include"prosesregris.php";
+	}
+?>
