877938f9d554374292079715532effdd4fbfc2b9
MUCHAMMAD RICHARD MAULANA <43516487+Richardmaulana@users.noreply.github.com>
2018-10-05 10:20:02 +0700

    komentar.php
    MUCHAMMAD RICHARD MAULANA
    6701174072

--- komentar.php
+++ komentar.php
@@ -1,3 +1,17 @@
+<form method="POST">
+	<font size="7" color="blue">KOMENTAR</font><br>
+	<textarea name="komentar"></textarea><br>
+	<input type="submit" name="kirim" value="Kirim">
+</form>
+	
 <?php
-
+	if (isset($_POST['kirim'])) {
+		$komen = $_POST['komentar'];
+		if (str_word_count($komen)>=5) {
+			echo $komen;
+		}
+		else{
+			echo "Komentar harus diatas 5 kalimat";		
+		}	
+	}
 ?>
